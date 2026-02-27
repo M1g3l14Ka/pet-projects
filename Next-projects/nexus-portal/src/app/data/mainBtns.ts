baedb45ea96bc329304249791c116362b16f0d33
Michael <147346501+M1g3l14Ka@users.noreply.github.com>
2026-02-27 04:02:05 +0300

    Delete directory

--- Next-projects/nexus-portal/src/app/data/mainBtns.ts
+++
@@ -1,9 +0,0 @@
-
-
-export const mainBtns = [
-  { id: 1, FactionType: 'IDE' },
-  { id: 2, FactionType: 'Frontend' },
-  { id: 3, FactionType: 'Backend' },
-  { id: 4, FactionType: 'GameDev' },
-  { id: 5, FactionType: 'Tools' },
-];
